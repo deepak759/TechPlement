95a13d793a531492fca514a6027fa91a2c1d7e01
Deepak <deepaksharma83407@gmail.com>
2024-01-28 23:56:47 +0530

    ui issuse fixed

--- week1-tasks/client/src/pages/Header.jsx
+++ week1-tasks/client/src/pages/Header.jsx
@@ -60,7 +60,7 @@ export default function Header() {
           <div className="flex items-center">
             <img
               src={logo}
-              className="w-48 rounded-full mt-32  md:mt-28 mx-auto"
+              className="w-48 rounded-full mt-[120px]  md:mt-28 mx-auto"
               alt="Logo"
             />
           </div>
